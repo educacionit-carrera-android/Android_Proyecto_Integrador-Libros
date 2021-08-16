7c0f2a560da3e36c07043689b87800149c16586d
Nicolas Diaz <nicolas@thelucky.app>
2021-08-16 01:06:42 -0300

    Add lab 1 for module 2

--- app/src/main/java/com/educacionit/libros/MainActivity.java
+++ app/src/main/java/com/educacionit/libros/MainActivity.java
@@ -1,14 +1,38 @@
 package com.educacionit.libros;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etUsuario;
+    private EditText etContrasenia;
+    private Button btnIniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupUI();
+    }
+
+    private void setupUI() {
+        etUsuario = findViewById(R.id.etUsuario);
+        etContrasenia = findViewById(R.id.etContraseña);
+        btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Iniciar sesión presionado", Toast.LENGTH_SHORT).show();
+                // TODO agregar acción correspondiente
+            }
+        });
     }
 }
