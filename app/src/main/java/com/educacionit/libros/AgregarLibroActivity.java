9cf1e5bfaf8c326726f36c9367005b5eed609753
Nicolas Diaz <nicolas@thelucky.app>
2021-08-18 21:47:10 -0300

    Add lab 3 for module 3

---
+++ app/src/main/java/com/educacionit/libros/AgregarLibroActivity.java
@@ -0,0 +1,14 @@
+package com.educacionit.libros;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AgregarLibroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_libro);
+    }
+}
--- app/src/main/java/com/educacionit/libros/HomeActivity.java
+++ app/src/main/java/com/educacionit/libros/HomeActivity.java
@@ -1,18 +1,52 @@
 package com.educacionit.libros;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        setupToolbar();
         saludarUsuario();
+    }
+
+    private void setupToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Mis Libros");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.item_agregar) {
+            goToAgregarLibro();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void goToAgregarLibro() {
+        startActivity(
+                new Intent(HomeActivity.this, AgregarLibroActivity.class)
+        );
     }
 
     private void saludarUsuario() {
