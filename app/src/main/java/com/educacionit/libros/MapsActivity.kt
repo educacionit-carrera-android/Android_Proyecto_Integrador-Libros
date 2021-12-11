a9155330f095d3a2c42a22cb448235f8dc9e0992
Nicolas Diaz <nicolas@thelucky.app>
2021-12-11 02:07:49 -0300

    Add lab 4 for module 8 - Avanzado

--- app/src/main/java/com/educacionit/libros/MapsActivity.kt
+++ app/src/main/java/com/educacionit/libros/MapsActivity.kt
@@ -1,8 +1,16 @@
 package com.educacionit.libros
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.educacionit.libros.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -10,11 +18,15 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
+private const val LOCATION_PERMISSION_CODE = 1
+private const val TAG = "MapsActivity"
 class MapsActivity : AppCompatActivity(),
     OnMapReadyCallback { // Implementa interfaz -> onMapReady()
 
     private lateinit var mMap: GoogleMap // Instancia del mapa para realizar acciones sobre él
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val locationPermission = Manifest.permission.ACCESS_FINE_LOCATION
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +34,8 @@ class MapsActivity : AppCompatActivity(),
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        callLocationPermission()
         // Obtiene la instancia del fragmento del mapa y define un listener para ser notificado
         // de cuando esté disponible para ser utilizado
         val mapFragment = supportFragmentManager
@@ -46,5 +60,69 @@ class MapsActivity : AppCompatActivity(),
         return MarkerOptions()
             .position(latLng)
             .title(title)
+    }
+
+    private fun callLocationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            when {
+                checkSelfPermission(locationPermission) == PackageManager.PERMISSION_GRANTED -> {
+                    // Permiso concedido previamente, avanzar con el flujo
+                    Log.i(TAG, "Permiso concedido previamente")
+                    getLastKnowLocation()
+                }
+                shouldShowRequestPermissionRationale(locationPermission) -> {
+                    Log.i(TAG, "Debe pedir permiso racional")
+                    // Mostrar una pantalla/vista/diálogo indicando por qué es importante que acepte el permiso
+                    requestPermissions(
+                        arrayOf(locationPermission),
+                        LOCATION_PERMISSION_CODE
+                    )
+                }
+                else -> {
+                    // Solicitar permiso
+                    Log.i(TAG, "Solicitar permiso")
+                    requestPermissions(
+                        arrayOf(locationPermission),
+                        LOCATION_PERMISSION_CODE
+                    )
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastKnowLocation() {
+        fusedLocationProviderClient
+            .lastLocation
+            .addOnSuccessListener { location ->
+                if (location != null) {
+                    Log.i(TAG, "Ubicación: ${location.latitude}, ${location.longitude} ")
+
+                }
+            }
+            .addOnFailureListener {
+                Log.e(TAG, "No se pudo obtener la ubicación", it)
+            }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_CODE -> {
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    Log.i(TAG, "Permiso aceptado")
+                    getLastKnowLocation()
+                } else {
+                    Log.i(TAG, "Permiso denegado")
+                    // Explicar al usuario que la funcionalidad a la que quiere acceder no esta
+                    // disponible ya que requiere la aprobación del permiso en cuestión
+                    if (!shouldShowRequestPermissionRationale(locationPermission)) {
+                        Log.i(TAG, "Permiso denegado de manera permanente")
+                    }
+                }
+            }
+        }
     }
 }
